2b1742a5077c4deb312cb2feb22919809774a30e
Georgia P <georgia.736185@nv.ccsd.net>
2020-09-23 20:36:38 +0000

    Completed the HowFarAway part of the assignment

--- src/main/java/HowFarAway.java
+++ src/main/java/HowFarAway.java
@@ -4,6 +4,26 @@ public class HowFarAway
 {
     public static void main(String[] args)
     {
-        // your code here.
+      //creation of scanner object to get user input
+        Scanner scanner = new Scanner(System.in);
+
+      //creation of varibles that holds as follows
+        //starting values (latitude then longitude)
+          System.out.print("Enter the latitude of the starting location: ");
+          double startLat = scanner.nextDouble();
+          System.out.print("Enter the longitude of the starting location: ");
+          double startLong = scanner.nextDouble();
+        //ending values  (latitude then longitude)
+          System.out.print("Enter the latitude of the ending location: ");
+          double endLat = scanner.nextDouble();
+          System.out.print("Enter the longitude of the ending location: ");
+          double endLong = scanner.nextDouble();
+
+      //create two GeoLocation objects to use for calculation
+        GeoLocation startLocation = new GeoLocation(startLat, startLong);
+        GeoLocation endLocation = new GeoLocation(endLat, endLong);
+
+      //compute and print the distance from startLocation and endLocation
+        System.out.println("The distance is " + startLocation.distanceFrom(endLocation) + " miles.");
     }
 }
